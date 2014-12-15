f4bbe74a36155c2a4d50430a3400a9788d97ddf9
Jade McGough <jade@thezets.com>
2014-12-15 01:51:20 -0800

    chapter 5.1 notes

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
+    // let is a "declarative statement" and doesn't return a value
+    // (technically it does - the semicolon throws away the value and returns unit)
     let y = 5i;
+    // an if is an expression, and returns a value
     let x = if y < 5 { "meow" } else { ":3" };
     println!("The value of x is: {}", x);
 }
